d7c9baa4f087efce15cd482aabdd6485efa1ab53
ioanb7 <ioanbiticu2@gmail.com>
2020-03-29 13:49:18 +0100

    Display changes in Market.go

--- calculator/Market.go
+++ calculator/Market.go
@@ -19,10 +19,22 @@ func NewMarket(marketType MarketType, handicap float32, selections []Selection, 
 }
 
 func (m Market) String() string {
-	var output string = fmt.Sprintf("%s (%f) [%s]", m.MarketType.String(), m.Handicap, m.Status.String())
+	var output string = ""
+
+	handicap := fmt.Sprintf("%.1f", m.Handicap)
+	if float32(int(m.Handicap)) == m.Handicap {
+		handicap = fmt.Sprintf("%.0f", m.Handicap)
+	}
+
+	output = fmt.Sprintf("%s (%s) [%s]", m.MarketType.String(), handicap, m.Status.String())
+	if m.Handicap < 0.5 {
+		output = fmt.Sprintf("%s [%s]", m.MarketType.String(), m.Status.String())
+	}
+
 	if len(m.HostTypes) > 0 {
 		var hostTypeArrayStringify = ScoutInfo.HostTypeArrayStringify{}
 		output += " " + hostTypeArrayStringify.String(m.HostTypes)
 	}
+
 	return output
 }
